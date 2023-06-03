a4bc95828e25032e455352bb835de73102a768bd
Yuta <yuta_mori@outlook.jp>
2023-06-03 19:58:53 +0900

    add only readme

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,10 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { readFileSync } from "fs";
+export const readme = readFileSync("./README.md").toString();
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("pulumi-yuta20230603");
 
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
