5bdd26491f64d4f908241714ec523051de4bb898
Yuta <yuta_mori@outlook.jp>
2023-06-08 01:01:23 +0900

    add db module

--- terraform/root-modules/prod/network/main.tf
+++ terraform/root-modules/prod/network/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 module "prod-network" {
-  source           = "../../child-modules/network"
+  source           = "../../../child-modules/network"
   NW-name          = "Prod-VPC"
   cidr             = "10.0.0.0/16"
   public_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
