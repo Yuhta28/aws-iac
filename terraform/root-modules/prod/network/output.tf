410b4734cc24c75f9972b7818f6ee5aee17b3517
Yuta <yuta_mori@outlook.jp>
2023-06-25 11:15:41 +0900

    output

---
+++ terraform/root-modules/prod/network/output.tf
@@ -0,0 +1,11 @@
+#output "VPC_info" {
+#  value = module.prod-network.VPC_info
+#}
+
+output "VPC_id" {
+  value = module.prod-network.VPC_info.vpc_id
+}
+
+output "VPC_DBsubnets" {
+  value = module.prod-network.VPC_info.database_subnets
+}
