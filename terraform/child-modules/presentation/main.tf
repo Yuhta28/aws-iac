44917ac5470a0d31c700bd55f17beb9e0e267c74
Yuta <yuta_mori@outlook.jp>
2023-06-29 17:48:18 +0900

    add provider

--- terraform/child-modules/presentation/main.tf
+++ terraform/child-modules/presentation/main.tf
@@ -12,6 +12,6 @@ module "security-group_web" {
   name          = var.web_SG-name
   description   = "Security Group for ALB"
   vpc_id        = "vpc-00f452c4ee7d20190"
-  ingress_rules = ["http-80-tcp", "http-443-tcp"]
+  ingress_rules = ["http-80-tcp"]
 
 }
