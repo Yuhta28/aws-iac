783bb133df96086d285c5a1cb74be8b22beab2e3
Yuta <yuta_mori@outlook.jp>
2023-06-29 18:22:50 +0900

    fix

--- terraform/child-modules/presentation/main.tf
+++ terraform/child-modules/presentation/main.tf
@@ -12,18 +12,11 @@ module "security-group_web" {
   name                = var.web_SG-name
   description         = "Security Group for ALB"
   vpc_id              = "vpc-00f452c4ee7d20190"
-  ingress_cidr_blocks = ["10.10.0.0/16"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
+      rule        = "http-80-tcp"
       cidr_blocks = "0.0.0.0/0"
     },
   ]
