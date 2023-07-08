9acb7ac1a5fc0f98330da591da779af3004b9d77
Yuta <yuta_mori@outlook.jp>
2023-07-08 16:28:13 +0900

    fix

--- terraform/root-modules/prod/presentation/main.tf
+++ terraform/root-modules/prod/presentation/main.tf
@@ -19,6 +19,5 @@ module "prod-presentation" {
   source                     = "../../../child-modules/presentation"
   web_SG-name                = "Web"
   remote_network_stack_state = "aws-terraform-nw-prod"
-  vpc_id = module.prod-presentation.stack_state.vpc_id
-  
+  vpc_id = module.prod-presentation.stack_state.VPC_id
 }
