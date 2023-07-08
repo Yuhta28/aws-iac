ce4055d7a3245c9941692bde2a203368bcc2f3b9
Yuta <yuta_mori@outlook.jp>
2023-07-08 15:49:54 +0900

    check output

--- terraform/child-modules/presentation/main.tf
+++ terraform/child-modules/presentation/main.tf
@@ -18,18 +18,3 @@ module "security-group_web" {
     "http-80-tcp"
   ]
 }
-
-data "terraform_remote_state" "pool_presentation_stack_state" {
-  backend = "remote"
-  config = {
-    hostname = "spacelift.io"
-    organization = "yuhta28"
-    workspaces = {
-      name = "AWS-Terraform-Presentation-Prod"
-    }
-  }
-}
-
-output "stack_state" {
-  value  = data.terraform_remote_state.pool_presentation_stack_state
-}
--- terraform/child-modules/presentation/output.tf
+++ terraform/child-modules/presentation/output.tf
@@ -1,3 +1,18 @@
 output "SG_Web_Info" {
   value = module.security-group_web
 }
+
+data "terraform_remote_state" "pool_presentation_stack_state" {
+  backend = "remote"
+  config = {
+    hostname = "spacelift.io"
+    organization = "yuhta28"
+    workspaces = {
+      name = "AWS-Terraform-NW-Prod"
+    }
+  }
+}
+
+output "stack_state" {
+  value  = data.terraform_remote_state.pool_presentation_stack_state
+}
