dc936918a577a743b1d22e7425b42d6264f079b2
Yuta <yuta_mori@outlook.jp>
2023-07-15 09:58:40 +0900

    fix module

--- terraform/child-modules/app/main.tf
+++ terraform/child-modules/app/main.tf
@@ -6,4 +6,19 @@ module "ecs_frontend_cluster" {
     name  = "containerInsights"
     value = "enabled"
   }
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+  cloudwatch_log_group_retention_in_days = 7
 }
