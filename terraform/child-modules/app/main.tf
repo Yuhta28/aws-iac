3c58968a36bfe3d9f3a1276b5274d92ac8ea36a5
Yuta <yuta_mori@outlook.jp>
2023-07-15 23:05:09 +0900

    del resource

--- terraform/child-modules/app/main.tf
+++ terraform/child-modules/app/main.tf
@@ -23,14 +23,14 @@ module "ecs_frontend_cluster" {
   cloudwatch_log_group_retention_in_days = 7
 }
 
-module "ecs_frontend_service" {
-  source = "terraform-aws-modules/ecs/aws//modules/service"
-
-  name        = var.service_name
-  cluster_arn = module.ecs_frontend_cluster.arn
-
-  cpu    = 1024
-  memory = 4096
-
-  family = var.task_def
-}
+#module "ecs_frontend_service" {
+#  source = "terraform-aws-modules/ecs/aws//modules/service"
+#
+#  name        = var.service_name
+#  cluster_arn = module.ecs_frontend_cluster.arn
+#
+#  cpu    = 1024
+#  memory = 4096
+#
+#  family = var.task_def
+#}
--- terraform/child-modules/presentation/main.tf
+++ terraform/child-modules/presentation/main.tf
@@ -1,35 +1,36 @@
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-
-  name               = var.ALB-name
-  load_balancer_type = "application"
-  vpc_id             = var.vpc_id
-  subnets            = var.ALB-subnets
-
-  security_group_rules = {
-    ingress_all_http = {
-      type        = "ingress"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "HTTP web traffic"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress_all_https = {
-      type        = "ingress"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "HTTPS web traffic"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress_all = {
-      type        = "egress"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-}
+#module "alb" {
+#  source  = "terraform-aws-modules/alb/aws"
+#  version = "~> 8.0"
+#
+#  name               = var.ALB-name
+#  load_balancer_type = "application"
+#  vpc_id             = var.vpc_id
+#  subnets            = var.ALB-subnets
+#
+#  security_group_rules = {
+#    ingress_all_http = {
+#      type        = "ingress"
+#      from_port   = 80
+#      to_port     = 80
+#      protocol    = "tcp"
+#      description = "HTTP web traffic"
+#      cidr_blocks = ["0.0.0.0/0"]
+#    }
+#    ingress_all_https = {
+#      type        = "ingress"
+#      from_port   = 443
+#      to_port     = 443
+#      protocol    = "tcp"
+#      description = "HTTPS web traffic"
+#      cidr_blocks = ["0.0.0.0/0"]
+#    }
+#    egress_all = {
+#      type        = "egress"
+#      from_port   = 0
+#      to_port     = 0
+#      protocol    = "-1"
+#      cidr_blocks = ["0.0.0.0/0"]
+#    }
+#  }
+#}
+#
