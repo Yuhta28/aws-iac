70278eb27264eeeb0d0420439973166f7b1cba95
Yuta <yuta_mori@outlook.jp>
2023-10-28 11:36:10 +0900

    fix remote state

--- terraform/child-modules/presentation/main.tf
+++ terraform/child-modules/presentation/main.tf
@@ -4,6 +4,7 @@ module "alb" {
 
   name               = var.ALB-name
   load_balancer_type = "application"
+  #terraform_remote_stateでNWリソースのstateファイル情報を参照している
   vpc_id             = data.terraform_remote_state.remote_network_stack_state.outputs.VPC_id
   subnets            = data.terraform_remote_state.remote_network_stack_state.outputs.VPC_PublicSubnets
 
