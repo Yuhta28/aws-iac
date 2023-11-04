67eee266c7f0f7ffda257e0a62fd1d082555fe77
Yuta <yuta_mori@outlook.jp>
2023-11-04 16:17:15 +0900

    add hostzone

---
+++ terraform/child-modules/domain/main.tf
@@ -0,0 +1,13 @@
+module "route53_zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    var.domain_name = {
+        comment = "personal domain"
+        tags = {
+            env = "production"
+        }
+    }
+  }
+}
---
+++ terraform/root-modules/prod/domain/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "prod-domain" {
+    source = "../../../child-modules/domain"
+    domain_name = "yuta-aws.work"
+}
