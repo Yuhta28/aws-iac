c790350057890b1a866cd25ce010fd0b40b23f90
Yuta <yuta_mori@outlook.jp>
2023-11-11 13:26:16 +0900

    create vpc in pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -3,3 +3,13 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { readFileSync } from "fs";
 export const readme = readFileSync("./README.md").toString();
+
+// create VPC resource in Pulumi TS
+const vpc = new awsx.ec2.Vpc("vpc", {
+    cidrBlock: "10.0.0.0/16",
+    enableDnsHostnames: true,
+    enableDnsSupport: true,
+    tags: {
+        Name: "pulumi-vpc",
+    }
+});
