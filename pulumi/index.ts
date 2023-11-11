1caaa2256bbd9c3593df3bb260b9823e5a03a557
Yuta <yuta_mori@outlook.jp>
2023-11-11 13:53:28 +0900

    create vpc in pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,14 +2,30 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { readFileSync } from "fs";
+import { type } from "os";
 export const readme = readFileSync("./README.md").toString();
 
 // create VPC resource in Pulumi TS
 const vpc = new awsx.ec2.Vpc("vpc", {
-    cidrBlock: "10.0.0.0/16",
-    enableDnsHostnames: true,
-    enableDnsSupport: true,
-    tags: {
-        Name: "pulumi-vpc",
-    }
+  cidrBlock: "172.16.0.0/16",
+  enableDnsHostnames: true,
+  enableDnsSupport: true,
+  subnetSpecs: [
+    {
+      type: awsx.ec2.SubnetType.Public,
+      name: "public",
+      cidrMask: 24,
+    },
+    {
+      type: awsx.ec2.SubnetType.Private,
+      name: "private",
+      cidrMask: 24,
+    },
+  ],
+  natGateways: {
+    strategy: awsx.ec2.NatGatewayStrategy.Single,
+  },
+  tags: {
+    Name: "pulumi-vpc",
+  },
 });
