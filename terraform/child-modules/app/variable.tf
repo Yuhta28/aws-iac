e8d3bb48f5fab6e7232e39caaf35cb7626a99f85
Yuta <yuta_mori@outlook.jp>
2023-12-03 19:26:20 +0900

    fix cluster

--- terraform/child-modules/app/variable.tf
+++ terraform/child-modules/app/variable.tf
@@ -2,14 +2,6 @@ variable "cluster_name" {
   type = string
 }
 
-variable "service_name" {
-  type = string
-}
-
-variable "task_def" {
-  type = string
-}
-
 variable "remote_presentation_stack_state" {
   type = string
 }
