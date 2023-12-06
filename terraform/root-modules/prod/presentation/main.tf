ec0e967e983878a5e7711de947f9574793276cee
Yuta <yuta_mori@outlook.jp>
2023-12-06 16:32:58 +0900

    dell alb

--- terraform/root-modules/prod/presentation/main.tf
+++ terraform/root-modules/prod/presentation/main.tf
@@ -1,16 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-module "prod-presentation" {
-  source                     = "../../../child-modules/presentation"
-  remote_network_stack_state = "aws-terraform-nw-prod"
-
-  ALB-name = "Prod-Web-ALB"
-
-}
+#module "prod-presentation" {
+#  source                     = "../../../child-modules/presentation"
+#  remote_network_stack_state = "aws-terraform-nw-prod"
+#
+#  ALB-name = "Prod-Web-ALB"
+#}
