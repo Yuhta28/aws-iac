fd2bdb8317497b4eabdc2f44cc346fbc0843dcf5
Yuta <yuta_mori@outlook.jp>
2023-12-06 17:49:30 +0900

    fix domain

--- terraform/root-modules/prod/domain/main.tf
+++ terraform/root-modules/prod/domain/main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-module "prod-domain" {
-  source      = "../../../child-modules/domain"
-  domain_name = "yuta-aws.work"
-}
+#module "prod-domain" {
+#  source      = "../../../child-modules/domain"
+#  domain_name = "yuta-aws.work"
+#}
