aa024419a1c06a8641863e832e9d4933485b2ab2
Yuta <yuta_mori@outlook.jp>
2023-12-14 23:41:04 +0900

    fix article

--- terraform/child-modules/domain/main.tf
+++ terraform/child-modules/domain/main.tf
@@ -2,22 +2,22 @@ resource "aws_route53_zone" "hostzone" {
   name = var.domain_name
 }
 
-#module "acm" {
-#  source  = "terraform-aws-modules/acm/aws"
-#  version = "~> 4.0"
-#
-#  domain_name = var.domain_name
-#  zone_id     = aws_route53_zone.hostzone.zone_id
-#
-#  validation_method = "DNS"
-#
-#  subject_alternative_names = [
-#    "*.${var.domain_name}"
-#  ]
-#
-#  validation_timeout = "10m"
-#
-#  tags = {
-#    Name = var.domain_name
-#  }
-#}
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name = var.domain_name
+  zone_id     = aws_route53_zone.hostzone.zone_id
+
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.${var.domain_name}"
+  ]
+
+  validation_timeout = "10m"
+
+  tags = {
+    Name = var.domain_name
+  }
+}
