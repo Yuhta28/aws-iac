99fa98302c9c29e062ee96aa87cee28558ca794b
Yuta <yuta_mori@outlook.jp>
2023-12-16 12:14:43 +0900

    run ec2

--- terraform/child-modules/app/remote_state.tf
+++ terraform/child-modules/app/remote_state.tf
@@ -8,3 +8,14 @@ data "terraform_remote_state" "remote_network_stack_state" {
     }
   }
 }
+
+data "terraform_remote_state" "remote_presentation_stack_state" {
+  backend = "remote"
+  config = {
+    hostname     = "spacelift.io"
+    organization = "yuhta28"
+    workspaces = {
+      name = var.remote_presentation_stack_state
+    }
+  }
+}
