726ac291d0d0b0bbc38f943b0b07cb639981c377
Yuta <yuta_mori@outlook.jp>
2024-04-28 13:39:11 +0900

    del nat

--- terraform/root-modules/prod/network/main.tf
+++ terraform/root-modules/prod/network/main.tf
@@ -14,6 +14,6 @@ module "prod-network" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
   database_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
 }
